c9d2ff6002f4bdb942fdf4733642db8f02e4e82b
omengoemmanuel <omengomanuel@gmail.com>
2024-10-04 15:29:29 +0300

    edits

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.urls import path
+
 
 # Create your views here.
+
+def cal(request):
+    return HttpResponse('welcome home')
